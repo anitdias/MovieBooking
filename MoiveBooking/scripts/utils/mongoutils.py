9eb1c67ac0c1fc8512bfb5b9173875eebc7e159b
anit.dias <anitdias3@gmail.com>
2024-08-20 11:58:49 +0530

    backend complete

--- MoiveBooking/scripts/utils/mongoutils.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from scripts.constants.app_constanst import Constants
-
-mongo_uri = Constants.mongo_uri
-database_name = Constants.db
-
-client = MongoClient(mongo_uri)
-
-db = client[database_name]
